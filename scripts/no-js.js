5cf32e80062b1b408fbdb9ef7a65d797e132244e
Kenua <jgvezon@gmail.com>
2021-08-13 16:28:33 -0500

    Project files added
    All files of tab project added.

---
+++ scripts/no-js.js
@@ -0,0 +1,2 @@
+'use strict';
+document.documentElement.className = document.documentElement.className.replace(/no-js/, ''); // remove 'no-js' class name
---
+++ scripts/tabs.js
@@ -0,0 +1,41 @@
+/*
+   TABS PROCESS
+      Reference every 'tab-content' element and 'tabs__button' and set the first content element and button to be the 
+         current elements in variables. This variables will be used to hide the previous content and button.
+
+      When a button is clicked, hide the current content and unselect the current button. Then show and select the 
+         current content and button.
+
+   TABS ANIMATION
+      When a tab is selected the current one should slide down while its opacity increases. 
+*/
+
+const tabButtonsContainer = document.querySelector('#tab__buttons-container');
+const tabButtons = document.querySelectorAll('.tab__button');
+const tabContentNodes = document.querySelectorAll('.tab-content');
+let currentTabButton = tabButtons[0];
+let currentTabContent = tabContentNodes[0];
+
+tabButtonsContainer.addEventListener('click', e => {
+   let target = e.target;
+
+   if (target.getAttribute('href')) { // check if target is an anchor
+      let id = target.getAttribute('href');
+
+      // # unselect previous tab content and button
+      currentTabButton.classList.remove('tab__button--selected');
+      currentTabContent.classList.remove('tab-content--show');
+      currentTabContent.classList.add('tab-content--hidden');
+
+      // # select current tab content and button
+      target.classList.add('tab__button--selected');
+      currentTabButton = target;
+      for (let i = 0; i < tabContentNodes.length; i++) {
+         if ('#'+tabContentNodes[i].id === id) {
+            tabContentNodes[i].classList.add('tab-content--show');
+            currentTabContent = tabContentNodes[i];
+            break;
+         }
+      }
+   }
+});
